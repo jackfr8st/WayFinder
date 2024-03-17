e13a2fc3f46b719cf1b215d3db459eceb4e84786
jackfr8st <86789457+jackfr8st@users.noreply.github.com>
2024-03-17 12:45:59 +0530

    Added Utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:wayfinder/utils/theme/theme.dart';
+import 'app.dart';
 
 void main() {
+
+  // To do: Add Widget bindings
+  // To do: Init local storage
+  // To do: await native splash
+  // To do: initialize firebase
+  // To do: initialize auth
+
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,  //sets the theme according to the theme of the system
-      /* this approach is messy
-      theme: ThemeData(),  // Default theme -> light
-      darkTheme: ThemeData(),
-      */
-
-      //instead make a separate class for the theme
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
-}
